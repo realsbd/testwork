fc8eaf34ea052970808fb7e4b0822c9725bc46f4
Abimbola Salawu <abimbola.salawudeen@gmail.com>
2022-12-12 17:01:08 -0500

    cleaning up

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { Head, Link } from '@inertiajs/inertia-vue3';
-import countries from '../Components/countries.json';
 // export default {
 //   data() {
 //     return {
@@ -43,42 +42,7 @@ defineProps({
                     class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</Link>
             </template>
         </div>
-        <div>
-            <form>
-                <div class="block w-1/2">
-                    <label for="name" class="text-sky-200">Full Name:</label>
-                    <input id="name" type="text"
-                        class="w-full my-2 py-2 px-3 rounded-lg focus:outline-none focus:shadow-outline"
-                        v-model="fullName" />
-                </div>
-
-                <div class="block w-1/2">
-                    <label for="email" class="text-sky-200">Email:</label>
-                    <input id="email" type="email"
-                        class="w-full my-2 py-2 px-3 rounded-lg focus:outline-none focus:shadow-outline"
-                        v-model="email" />
-                </div>
-
-                <div class="block w-1/2">
-                    <label for="country" class="text-sky-200">Country:</label>
-                    <select id="country" v-model="selectedCountry" @change="updatePhonePrefix">
-                        <option v-for="country in countries" :key="country.code" :value="country">
-                            {{ country.name }}
-                        </option>
-                    </select>
-                    <!-- <img :src="selectedCountry.flag" :alt="selectedCountry.name" /> -->
-                </div>
-
-                <div class="block w-1/2">
-                    <label for="phone" class="text-sky-200">Phone Number:</label>
-                    <input id="phone" type="tel"
-                        class="w-full my-2 py-2 px-3 rounded-lg focus:outline-none focus:shadow-outline"
-                        v-model="phoneNumber" />
-                </div>
-
-                <button class="py-2 px-4 bg-blue-500 text-white rounded-lg" @click.prevent="submitForm">Submit</button>
-            </form>
-        </div>
+        
     </div>
 </template>
 
